e909c4c67f1ec6c76f70f0b37dea21c277f9058e
Robervan Souza <rob.robervan@hotmail.com>
2022-12-24 21:12:41 -0300

    aula 03 concluida

---
+++ dia_03.py
@@ -0,0 +1,108 @@
+# # Controle de fluxo
+# def controle_de_idade(username, age, with_parents):
+#     if age < 18:
+#         msg = f"Desculpe {username}, mas você não pode assistir filmes"
+#         if with_parents == 'sim':
+#             msg = f" Você  {username} pode assistir"
+#         else:
+#             msg = f"Desculpe {username}, mas você não pode assistir filmes"
+#     else:
+#         msg = f"{username}, você pode assistir filmes"
+#     print(msg)
+#     return msg
+# n1 = input('digite seu nome: ')
+# n2 = int(input('digite sua idade: '))
+# n3 = input('voce esta com seua pais, digite sim ou não: ')
+# controle_de_idade(n1, n2, n3)
+#
+#
+# def controle_de_idade(username, age, with_parents):
+#     if age >= 18 or with_parents == 'sim':
+#         msg = f"{username}, você pode assistir filmes"
+#     else:
+#         msg = f"Desculpe {username}, mas você não pode assistir filmes"
+#     print(msg)
+#     return msg
+#
+#
+# n1 = input('digite seu nome: ')
+# n2 = int(input('digite sua idade: '))
+# n3 = input('voce esta com seus pais, digite sim ou não: ')
+# controle_de_idade(n1, n2, n3)
+###################333333##################################################################################################3
+# # For loop
+# students = [{'name': 'robervan', 'score': 8},
+#             {'name': 'auriene', 'score': 7},
+#              {'name': 'alice', 'score': 5},
+#              {'name': 'beatriz', 'score': 5}]
+# aprovados = []
+# reprovados = []
+# for student in students:
+#      if student["score"] > 6:
+#          aprovados.append(student)
+#      else:
+#          reprovados.append(student)
+#
+# print('aprovados', aprovados)
+# print('reprovados', reprovados)
+
+# students = [{'name': 'robervan', 'score': 8},
+#             {'name': 'auriene', 'score': 7},
+#               {'name': 'alice', 'score': 5},
+#              {'name': 'beatriz', 'score': 5}]
+#
+# for i in students:
+#     if i ['score'] < 6:
+#         print('aluno reprovado')
+#         break
+#     else:
+#         print(i['name'])
+#
+# for i in students:
+#     if i ['score'] > 6:
+#         print('aluno aprovado')
+#         continue
+#         print(i['name'])
+
+################################################################
+# Modulos nos modulos tem que importar uma função de outa pagina.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
